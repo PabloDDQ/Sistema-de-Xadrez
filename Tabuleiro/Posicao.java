114f02ad1ee80e1c88a6f6ff778bee9033cde56a
PabloDDQ <160532715+PabloDDQ@users.noreply.github.com>
2025-01-01 16:55:26 -0300

    Adicionado a classe posição

---
+++ Tabuleiro/Posicao.java
@@ -0,0 +1,31 @@
+public class Posicao {
+
+    private int linha;
+    private int coluna;
+
+    public Posicao(int coluna, int linha) {
+        this.coluna = coluna;
+        this.linha = linha;
+    }
+
+    public int getLinha() {
+        return linha;
+    }
+
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    public int getColuna() {
+        return coluna;
+    }
+
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+
+    @Override
+    public String toString(){
+        return linha + ", " + coluna;
+    }
+}
